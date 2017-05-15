1094689dc2f310c66bd39f3bc74c9413e506e073
Kevin_Feng <Kevin_Feng@139.com>
2017-05-15 12:31:00 +0800

    finish DataCollection

--- DataCollection/getdata.php
+++ DataCollection/getdata.php
@@ -1,17 +1,19 @@
 <?php
 $date=$_GET["date"];
-$sleeptime=$_GET["sleeptime"];
-$waketime=$_GET["waketime"];
-$sleepplace=$_GET["sleepplace"];
-$amq=$_GET["amq"];
-$amplace=$_GET["amplace"];
-$amnote=$_GET["amnote"];
+$sleeptime=$_GET["sleeptime"];#time when you start sleep
+$waketime=$_GET["waketime"];#time when you wake up
+$sleepplace=$_GET["sleepplace"];#place where you sleep
+$amc=$_GET["amc"];
+$amq=$_GET["amq"];#quality in am
+$amplace=$_GET["amplace"];#place in am
+$amnote=$_GET["amnote"];#someting happen in am
+$pmc=$_GET["pmc"];
 $pmq=$_GET["pmq"];
 $pmplace=$_GET["pmplace"];
 $pmnote=$_GET["pmnote"];
-$newdata = $date.", ".$sleeptime.", ".$waketime.", ".$sleepplace.", ".$amq.", ".$amplace.", ".$amnote.", ".$pmq.", ".$pmplace.", ".$pmnote."\n";
+$newdata = $date.", ".$sleeptime.", ".$waketime.", ".$sleepplace.", ".$amc.", ".$amq.", ".$amplace.", ".$amnote.", ".$pmc.", ".$pmq.", ".$pmplace.", ".$pmnote."\n";
 echo $newdata;
-$datafile = fopen("/sleepdata.data", "a+");
+$datafile = fopen("/sleepdata.data", "a+");# record data
 fwrite($datafile,$newdata);
 fclose($datafile);
 ?>
